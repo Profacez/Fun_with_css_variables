bf344a6a981a0a6af961b74fbc7d8b4c05924de9
baltazar <arielochefe80@gmail.com>
2024-02-06 22:18:05 -0300

    adding js

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
+
+inputs.forEach(element => element.addEventListener('change', handleUpdate));
+inputs.forEach(element => element.addEventListener('mousemove', handleUpdate));
